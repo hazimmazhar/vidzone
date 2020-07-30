89d95042a4c4d2415ee2c2fe810572419dae5a62
Samar-Shah <samarshah14@gmail.com>
2020-07-30 11:52:11 +0500

    First Commit

---
+++ routes/customers.js
@@ -0,0 +1,69 @@
+const auth = require("../middleware/auth");
+const { Customer, validate } = require("../models/customer");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+router.get("/", auth, async (req, res) => {
+  const customers = await Customer.find().sort("name");
+  res.send(customers);
+});
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const customer = await Customer.findById(req.params.id);
+    res.send(customer);
+  } catch (error) {
+    return res.status(404).send("Customer does not exist");
+  }
+});
+
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const customer = await new Customer({
+    name: req.body.name,
+    phone: req.body.phone,
+    isGold: req.body.isGold,
+  });
+  await customer.save();
+
+  res.send(customer);
+});
+
+router.put("/:id", auth, async (req, res) => {
+  const { error } = validate(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  try {
+    const customer = await Customer.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        phone: req.body.phone,
+        isGold: req.body.isGold,
+      },
+      { new: true }
+    );
+    res.send(customer);
+  } catch (error) {
+    return res.status(404).send("The customer was not found");
+  }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+    res.send(customer);
+  } catch (error) {
+    return res.status(404).send("The customer was not found");
+  }
+});
+
+module.exports = router;
