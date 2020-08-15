9a9896fd767056a335615dbbc236138cc13a4fa3
Hazim <hazimmazhar13@gmail.com>
2020-08-15 19:40:59 +0500

    added cors package

--- startup/prod.js
+++ startup/prod.js
@@ -1,7 +1,9 @@
 const helmet = require("helmet");
+const cors = require("cors")
 const compression = require("compression");
 
 module.exports = function (app) {
   app.use(helmet());
   app.use(compression());
+  app.use(cors());
 };
